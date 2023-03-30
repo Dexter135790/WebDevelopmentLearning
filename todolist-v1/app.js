086344dfbebd1636785ac34cbc4bd31766ef65b5
DhirajBarela12 <202152310@iiitvadodara.ac.in>
2023-03-30 22:53:43 +0530

    Added mongoose and added default values

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js");
-
+const mongoose = require('mongoose');
 
 const app = express();
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended : true }))
@@ -13,12 +10,46 @@ app.use(bodyParser.urlencoded({ extended : true }))
 //To use the public forlder for the other resources 
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost/toDoListDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB', err));
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to to do list!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new Item"
+});
+
+const item3 = new Item({
+    name: "<- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems)
+.then(()=>{
+    console.log("Successfully inserted!");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+
+
 app.get("/", (req, res)=>{
     
-    let day = date.getDay();
     // Render the variables in the ejs code 
     res.render("list", {
-        listTitle: day,
+        listTitle: "Today",
         newListItems: items
     });
 });
